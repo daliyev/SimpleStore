e516af6863349e1f46f4abc864313fe618198b81
daliyev <daliyevazizbekk@gmail.com>
2024-07-19 08:24:27 +0500

    database change to postgresql

--- SimpleStore/settings.py
+++ SimpleStore/settings.py
@@ -89,8 +89,13 @@ WSGI_APPLICATION = 'SimpleStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        # 'HOST': 'db',
+        'HOST': 'localhost',
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
